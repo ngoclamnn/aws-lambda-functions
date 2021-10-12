36d262300e90dabf5ce77a5474776f8ec225100c
ngoclamnn <ngoclamnn@gmail.com>
2021-10-12 11:23:36 +0700

    add code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sql = require('mssql')
-var AWS = require("aws-sdk")
-const SNS_TOPIC = process.env.SNS_TOPIC_ARN;
+const https = require('https');
+const { URLSearchParams } = require('url');
 const sqlConfig = {
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
@@ -12,6 +12,9 @@ const sqlConfig = {
     trustServerCertificate: false
   }
 }
+
+
+
 
 exports.handler = async (event, context) => {
   sql.connect(sqlConfig, err => {
@@ -34,16 +37,61 @@ exports.handler = async (event, context) => {
         <title>Welcome Email</title>
         </head>
         <body>
-        <h2>Hello Sammy</h2>
+        <h2>Hello Everyone,</h2>
         <p>Here is the GCMS approval data. </p>
         ${htmlTable}
         </body>
         </html>`
       console.log(content)
+      sendEmail(content)
     })
+ 
   })
 
   sql.on('error', err => {
     console.log(err)
   })
+}
+
+function sendEmail(htmlContent){
+  console.log('sendEmail');
+  const params = {
+    from: 'primera@partnersgroup.com',
+    to: 'c.axonactive.nguyenngoc@partnersgroup.com',
+    subject: "GCMS approval status",
+    body: htmlContent,
+    sourceSystem: 'AWS CLOUDWATCH ALERT',
+    processorDescription: 'LAMBDA ALERT NOTIFICATION',
+    createdBy: "LAMBDA GCMS ALERT NOTIFICATION"
+  }
+  var postData = new URLSearchParams(params);
+  var options = {
+    hostname: 'primeramail.apps.partnersgroup.net',
+    port: 443,
+    path: '/rest/addMail',
+    method: 'POST',
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false,
+    headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+         'Content-Length': postData.length
+       }
+  };
+  
+  var req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+  
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+  
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  
+  req.write(postData);
+  req.end();
 }
--- test.js
+++ test.js
@@ -8,8 +8,8 @@ const sqlConfig = {
     server: "localhost",
     port: 1433,
     options: {
-        encrypt: false, 
-        trustServerCertificate: false 
+        encrypt: false,
+        trustServerCertificate: false
     }
 }
 async function main() {
@@ -33,17 +33,62 @@ async function main() {
             <title>Welcome Email</title>
             </head>
             <body>
-            <h2>Hello Sammy</h2>
+            <h2>Hello Everyone,</h2>
             <p>Here is the GCMS approval data. </p>
             ${htmlTable}
             </body>
             </html>`
             console.log(content)
+            sendEmail(content)
         })
+
     })
 
     sql.on('error', err => {
         console.log(err)
     })
+}
+function sendEmail(htmlContent) {
+    console.log('Start Sending Email...');
+    const params = {
+        from: 'primera@partnersgroup.com',
+        to: 'c.axonactive.nguyenngoc@partnersgroup.com',
+        subject: "GCMS approval status",
+        body: htmlContent,
+        sourceSystem: 'AWS CLOUDWATCH ALERT',
+        processorDescription: 'LAMBDA ALERT NOTIFICATION',
+        createdBy: "LAMBDA GCMS ALERT NOTIFICATION"
+    }
+    var postData = new URLSearchParams(params).toString();
+    var options = {
+        hostname: 'primeramail.apps.partnersgroup.net',
+        port: 443,
+        path: '/rest/addMail',
+        method: 'POST',
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': postData.length
+        }
+    };
+
+    var req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+        console.log("Done Sending Email.");
+        res.on('data', (d) => {
+            console.log("Response", d);
+            
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+
+    req.write(postData);
+    req.end();
 }
 main();
